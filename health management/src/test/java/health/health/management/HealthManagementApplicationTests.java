83c6e00c0ea6aef1a75d4f1fb6a5241ff9211333
MyunghyungNero <mh1kim0910@gmail.com>
2022-03-07 01:51:29 +0900

    health management

---
+++ health management/src/test/java/health/health/management/HealthManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package health.health.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
