93cdb4b1a835f1d1f047f85b656981414266c230
MyunghyungNero <mh1kim0910@gmail.com>
2022-05-16 23:54:44 +0900

    ...

---
+++ health-management-service2/health-management-service2/src/main/java/healthmanagementservice2/healthmanagementservice2/HealthManagementService2Application.java
@@ -0,0 +1,13 @@
+package healthmanagementservice2.healthmanagementservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthManagementService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthManagementService2Application.class, args);
+	}
+
+}
---
+++ health-management-service2/health-management-service2/src/test/java/healthmanagementservice2/healthmanagementservice2/HealthManagementService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package healthmanagementservice2.healthmanagementservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthManagementService2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
