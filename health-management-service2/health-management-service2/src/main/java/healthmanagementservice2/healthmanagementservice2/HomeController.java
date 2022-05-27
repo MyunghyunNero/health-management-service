4ad0e256e2083cbb069bec1d1835357011c0a0b6
MyunghyungNero <mh1kim0910@gmail.com>
2022-05-28 00:54:06 +0900

    homecontroller

--- health-management-service2/health-management-service2/src/main/java/healthmanagementservice2/healthmanagementservice2/HomeController.java
+++ health-management-service2/health-management-service2/src/main/java/healthmanagementservice2/healthmanagementservice2/HomeController.java
@@ -1,9 +1,25 @@
 package healthmanagementservice2.healthmanagementservice2;
 
+import healthmanagementservice2.healthmanagementservice2.Login.LoginForm;
+import healthmanagementservice2.healthmanagementservice2.Login.SessionConst;
+import healthmanagementservice2.healthmanagementservice2.domain.Member;
+import healthmanagementservice2.healthmanagementservice2.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+    private final MemberService memberService;
+    @GetMapping("/")
+    public String homeLogin(@SessionAttribute(name = SessionConst.LOGIN_MEMBER,required = false)
+                            Member loginmember, Model model){
+        if(loginmember==null){
+            return "home";
+        }
+        return "home"; //이따가 수정
+    }
 }
